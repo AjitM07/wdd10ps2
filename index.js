fad6e79b32a12507cd570b2a25f78b9dfd91e25f
AjitM07 <ajitmangsulikar123@gmail.com>
2026-04-23 18:19:12 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("Application Deployed Successfully");
+})
+
+app.get('/status', (req, res) => {
+    res.json({
+        status: "running",
+        environment: process.env.NODE_ENV || "development"
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+
